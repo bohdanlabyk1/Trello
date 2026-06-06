b9fd1b840e8fcd6dfe98abefc9c223d5b4a46c9d
Labyk Bohdan <138875140+bohdanlabyk1@users.noreply.github.com>
2026-06-06 17:26:34 +0300

    Refactor database connection to use environment

--- back-end/src/database/database.module.ts
+++ back-end/src/database/database.module.ts
@@ -14,14 +14,16 @@ import { Notification } from 'src/invitation/noification.entiti';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306, 
-      username: 'root', 
-      password: '1111', 
-      database: 'teamtrack',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 3306, 
+      username: process.env.DB_USERNAME || 'root', 
+      password: process.env.DB_PASSWORD || '1111', 
+      database: process.env.DB_DATABASE || 'teamtrack',
       autoLoadEntities: true,
       synchronize: true,
       entities: [Auth, Project, ColumnEntity, Comment, Task, Invitation, Sprint, ActivityLog, Notification], 
+      // Багато хмарних MySQL вимагають SSL підключення. Додайте цей блок:
+      ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : false,
     }),
     TypeOrmModule.forFeature([Auth, Project, ColumnEntity, Comment, Task, Invitation, Sprint, ActivityLog, Notification]),
   ],
